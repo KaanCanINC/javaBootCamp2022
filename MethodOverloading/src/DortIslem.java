f513f9ae7caf220476db2e2d42b2abf9bb823564
example <email@example.com>
2022-09-28 15:21:53 +0300

    Initial commit

---
+++ MethodOverloading/src/DortIslem.java
@@ -0,0 +1,8 @@
+public class DortIslem {
+    public int topla(int sayil, int sayi2){
+        return sayil+sayi2;
+    }
+    public int topla(int sayil, int sayi2, int sayi3){
+        return sayil+sayi2;
+    }
+}
